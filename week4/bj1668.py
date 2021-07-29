9d88106fd3dba55eefecd8a3c38c3d38c54745fa
SXL-SXO <86880813+SXL-SXO@users.noreply.github.com>
2021-07-30 02:32:06 +0900

    Add files via upload

---
+++ week4/bj1668.py
@@ -0,0 +1,27 @@
+import sys
+
+N=sys.stdin.readline()
+N=int(N)
+
+turopi=[]
+for b in range(N):
+    turopi.append(sys.stdin.readline())
+
+big_f=int(turopi[0])
+big_b=int(turopi[N-1])
+
+look_f=1
+look_b=1
+
+for a in range(N):
+    turopi[a]=int(turopi[a])
+    turopi[N-1-a]=int(turopi[N-1-a])
+    if big_f<turopi[a]:
+        big_f=turopi[a]
+        look_f+=1
+    if big_b<turopi[N-1-a]:
+        big_b=turopi[N-1-a]
+        look_b+=1
+
+print(look_f)
+print(look_b)    
