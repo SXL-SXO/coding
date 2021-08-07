1dd280b00055bf060b7e80727cfb85cc2248b4ef
SXL-SXO <86880813+SXL-SXO@users.noreply.github.com>
2021-08-08 01:13:51 +0900

    Update bj18511(20210729).py

--- week4/bj18511(20210729).py
+++ week4/bj18511(20210729).py
@@ -1,4 +1,50 @@
 #N ,K을 입력받고 K만큼(1~3) K로 만들수 있는 N보다 작은 수를 프린트하기
+import sys
+from collections import deque
+N, K=sys.stdin.readline().split()
+kkk=sys.stdin.readline().split()
+
+leng=len(N)
+K=int(K)
+N=int(N)
+
+for a in range(K):
+    kkk[a]=int(kkk[a])
+ 
+s_kkk=sorted(kkk)
+
+error=0
+result=0
+where=leng-1
+
+while(1):
+    if where==-1:
+        print(result)
+        break
+    for i in range(K,0,-1):
+        if result+s_kkk[i-1]*10**where<=N:
+            result=result+s_kkk[i-1]*10**where
+            where-=1            
+            break
+        if i==1:
+            error=1
+    if error==1:
+        if where==leng-1:
+            result=0
+            where-=1
+            error=0
+            continue
+        for j in range(K,0,-1):
+            if (result//10**(where+1))-(result//10**(where+2))*10==s_kkk[j-1]:
+                if j==1:
+                    where+=1
+                    break                        
+                result=(result//10**(where+2))*(10**(where+2))+s_kkk[j-2]*10**(where+1)
+                error=0
+                brea
+
+'''
+#무조건 자릿수를 줄여서 틀림
 bigx=0
 def num(s_kkk,leng,result,n,k,ori): #(정렬된배열, 이 자릿수의 값을 구한다, 최소값, n, k, 제일 작은 것을 만들때 len을 다시쓰기 힘드므로)
     global bigx
@@ -31,6 +77,6 @@ for a in range(K):
 s_kkk=sorted(kkk)
 
 num(s_kkk,leng-1,0,N,K,leng-1,)
-
+'''
 
             
