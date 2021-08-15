1e11066b63599191f238366217b19b52d8c54203
SXL-SXO <86880813+SXL-SXO@users.noreply.github.com>
2021-08-16 00:14:35 +0900

    Update group06(20210815).py

--- week6/group06(20210815).py
+++ week6/group06(20210815).py
@@ -1,5 +1,5 @@
 #카카오채용연계 2021 거리두기문제
-#테스트3번에서 오류가 남
+#테스트3번에서 오류가 남 POP로 대각선이 아닌 방향으로 앉는 경우를 고려하지 못했음.
 from collections import deque
 def check(where):
     meeting=[[]*x for x in range(5)]
@@ -16,6 +16,28 @@ def check(where):
     y_next=[1,-1,0,0,1,-1,1,-1]
     while(inter):
         m,n=inter.pop()
+        for i in range(12):
+            if poss==0:
+                break
+            if  m+x_next[i]<0 or m+x_next[i]>4 or n+y_next[i]<0 or n+y_next[i]>4:
+                    continue
+            if meeting[m+x_next[i]][n+y_next[i]]=='P':       
+                if i<4:
+                    poss=0
+                    break
+                if i>3 and i<8:
+                    if meeting[m+x_next[i]][n]!='X' or meeting[m][n+y_next[i]]!='X':
+                        poss=0
+                        break
+                if i>7 and i<10:
+                    if meeting[m][n+y_next[i]//2]!='X':
+                        poss=0
+                        break
+                if i>9:
+                    if meeting[m+x_next[i]//2][n]!='X':
+                        poss=0
+                        break
+        '''
         for i in range(8):
             if  m+x_next[i]==-1 or m+x_next[i]==5 or n+y_next[i]==-1 or n+y_next[i]==5: 
                     continue
@@ -26,6 +48,7 @@ def check(where):
                 if meeting[m+x_next[i]][n]!='X' or meeting[m][n+y_next[i]]!='X':
                     poss=0
                     break
+        '''
     return poss
 def solution(places):
     answer = []    
