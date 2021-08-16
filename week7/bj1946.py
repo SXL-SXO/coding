f3071ca29263c7697238c263a3694ff9b74c01d5
SXL-SXO <86880813+SXL-SXO@users.noreply.github.com>
2021-08-17 00:41:04 +0900

    Update bj1946.py

--- week7/bj1946.py
+++ week7/bj1946.py
@@ -1,3 +1,8 @@
+#입사면접을 봤는데 서류도 면접도 다른 지원자보다 등수가 낮으면 무조건 탈락한다. 이때 최대 합격자는?
+#나는 for문을 2번 돌려서 다른사람들과 하나하나 비교하는 방식을 썼더니 시간초과가 떴다. 
+#그래서 sort로 [0]의 등수를 순서대로 해서 앞등수보다 뒤에 등수가 [1]도 크면 탈락한다
+#이때 1등과 꼴등을 동시에 할 수 있기때문에 best는 N+1에서 시작해야 한다
+
 import sys
 from collections import deque
 
@@ -11,7 +16,7 @@ for z in range(T):
     poss=0
     imposs=0
 
-    best=N
+    best=N+1
     sample=[]
     
     for _ in range(N):
