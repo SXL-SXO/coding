7f5d5db3ef20a862359ed093053ba4496862e586
SXL-SXO <86880813+SXL-SXO@users.noreply.github.com>
2021-08-19 00:12:17 +0900

    Update bj9440.py

--- week7/bj9440.py
+++ week7/bj9440.py
@@ -1,3 +1,7 @@
+# 줄로 N 그다음에 n개만큼 숫자를 입력받아 이 수를 모두 사용해서 만들 수 있는 두 개의 수를 더해주는것
+# 일반적 상황: N을 입력받아 N만큼 받은 수를 오름차순으로 정렬하고 big small을 바꿔가면서 더해줘 작은 수를 만든다
+# 예외 상황: 0이 있어서 0은 처음자리로 갈 수 없으므로 한칸씩 이동해서 계산하는 경우 이때 N이 짝수인지, 0이 짝수개인지 고려해주어야한다
+
 import sys
 from collections import deque
 
@@ -7,10 +11,10 @@ while(1):
     N=int(read[0])
     if N==0:
         break 
-    big=0
-    small=0
+    big=0 #만드는 큰 수 
+    small=0 #만드는 작은 수
     zero=0    
-    i=0
+    i=0 #짝수개면 i와 j가 같은 수로 같은 자리 수 홀수개면 j가 하나 작음
     j=0
     for _ in range(N):        
         number.append(int(read.pop()))
